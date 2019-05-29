7cd779af4df0c16f3def8579f530f777db615ed9
Romain Freydiger <romain.freydiger@outlook.fr>
2019-05-29 14:12:09 +0200

    ng g library @library-storybook/a --prefix=a

---
+++ projects/library-storybook/a/src/lib/a.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'a-a',
+  template: `
+    <p>
+      a works!
+    </p>
+  `,
+  styles: []
+})
+export class AComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/library-storybook/a/src/lib/a.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { AComponent } from './a.component';
+
+@NgModule({
+  declarations: [AComponent],
+  imports: [
+  ],
+  exports: [AComponent]
+})
+export class AModule { }
